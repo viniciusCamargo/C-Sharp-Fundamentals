d8234e7ca22c5c5c79e51610d162d04fecea3118
Vinicius Camargo <vinicius.camargo@tua.com.br>
2016-02-16 17:22:30 -0200

    Arrays

--- Program.cs
+++ Program.cs
@@ -16,9 +16,18 @@ namespace Grades
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:C}");
         }
     }
 }
