c4de7c20e203f08cbf0492354763b2ec680f4ac3
Vinicius Camargo <vinicius.camargo@tua.com.br>
2016-02-16 16:30:41 -0200

    Added some tests

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -8,8 +8,72 @@ using System.Threading.Tasks;
 namespace Grades.Tests.Types
 {
     [TestClass]
-    public class ReferenceTypeTests
+    public class TypeTests
     {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            //grades = new float[5];
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UpperCaseString()
+        {
+            string name = "vinicius";
+            name = name.ToUpper();
+
+            Assert.AreEqual("VINICIUS", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2015, 1, 1);
+            date = date.AddDays(1);
+
+            Assert.AreEqual(2, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(x);
+
+            Assert.AreEqual(46, x);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number = number + 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
         [TestMethod]
         public void StringComparisons()
         {
